884a191cdcc7ed54d7a470993f8d9396aa0eb804
David Paulson <dpaul@microsoft.com>
2021-03-07 09:43:47 -0600

    Added Pester Testing to the build pipeline

---
+++ .build/PesterTesting.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding()]
+param (
+
+)
+
+$repoRoot = Get-Item "$PSScriptRoot\.."
+
+$scriptFiles = Get-ChildItem -Path $repoRoot -Directory |
+    Where-Object { $_.Name -ne ".build" } |
+    ForEach-Object { Get-ChildItem -Path $_.FullName *.ps1 -Recurse } |
+    Where-Object { $_.Name.Contains(".Tests.ps1") }
+    Sort-Object Name |
+    ForEach-Object { $_.FullName }
+
+$scriptFiles |
+    ForEach-Object {
+        Invoke-Pester $_
+    }
